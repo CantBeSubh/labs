93a4b9130b42ab5a1af7355af32a4f86e6b3186a
Subhranshu Pati <subhranshupati0412@gmail.com>
2024-09-17 00:51:37 +0530

    feat: macros

---
+++ rust/rust-learning-path/macros/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Hello, MACROSSSSS!");
+    println!(
+        "The first letter of the English alphabet is {} and the last letter is {}.",
+        'A', 'Z'
+    );
+    todo!("Display the message by using the println!() macro");
+}
